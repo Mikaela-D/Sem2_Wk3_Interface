04a07055e6b907a816279857ad19841949ecc8ed
Mikaela-D <G00401392@atu.ie>
2023-02-02 11:54:20 +0000

    Added class code to Product, Music and Product DB

--- src/main/java/ie/atu/productv4/Music.java
+++ src/main/java/ie/atu/productv4/Music.java
@@ -1,5 +1,28 @@
 package ie.atu.productv4;
 
-public class Music extends ProductDB{
+public class Music extends Product{
+    private String artist;
+    private String label;
 
+    public Music(){
+        super();
+        artist = "";
+        label = "";
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
 }
--- src/main/java/ie/atu/productv4/Product.java
+++ src/main/java/ie/atu/productv4/Product.java
@@ -1,4 +1,52 @@
 package ie.atu.productv4;
 
 public class Product {
+    private String price;
+    private String description;
+    private String code;
+    private String artist;
+    private String label;
+
+    public Product(){
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
 }
